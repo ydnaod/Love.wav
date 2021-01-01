e690a61867727c48004887a3f1c76017eeb46ad4
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-01-01 11:41:07 -0500

    add navbar structure and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React, {useState, useEffect, Fragment} from 'react';
+import {NavBar} from './Components/NavBar/NavBar';
+import {MessageList} from './Components/MessageList/MessageList';
+import {MatchDashboard} from './Components/MatchDashboard/MatchDashboard';
 
 function App() {
   return (
     <Fragment>
-      
+      <NavBar />
     </Fragment>
   );
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,19 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import './NavBar.css';
 
-export function NavBar(){
+export function NavBar() {
     return (
         <Fragment>
-
+            <div className="NavBar">
+                <div className="logo">
+                    <h1 id="logo">love.wav</h1>
+                </div>
+                <nav>
+                    <a href="/">settings</a>
+                    <a href="/">account</a>
+                    <a href="/">logout</a>
+                </nav>
+            </div>
         </Fragment>
     )
 }
