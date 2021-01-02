f97ade2cf213049414c36cbf6030986201ff4673
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-01-02 12:30:21 -0500

    add styling to profile component in match dashboard

--- src/Components/MatchDashboard/MatchDashboard.js
+++ src/Components/MatchDashboard/MatchDashboard.js
@@ -5,10 +5,35 @@ import playButton from '../../images/PlayButton(swiperight).png'
 import stopButton from '../../images/StopButton.png'
 
 export function MatchDashboard() {
+
+    const [profile, setProfile] = useState([]);
+
+    useEffect(() => {
+        try {
+            const profileInfo = [
+                {
+                    id:1,
+                    slideTitle: 'theme song',
+                    image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+                    title: '"Adore You"',
+                    artist: 'Harry Styles'
+                }
+            ]
+            setProfile(profileInfo);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }, [])
+
     return (
         <Fragment>
             <div className="MatchDashboard">
-                <Profile />
+                {
+                    profile.map(profile => {
+                    return <Profile profileInfo={profile}
+                    key={profile.id}/>})
+                    
+                }
                 <div>
                     <h1 className="name">Summer L.</h1>
                     <div className = "buttons">
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,20 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import './Profile.css';
 
-export function Profile(){
+export function Profile(props) {
     return (
         <Fragment>
             <div className="Profile">
-                
+                <div className="slideTitle">
+                    <h2>{props.profileInfo.slideTitle}</h2>
+                </div>
+                <div className="songDetailsSlide">
+                    <img className="albumCover" src={props.profileInfo.image} />
+                    <div className="songDetails">
+                        <h3>{props.profileInfo.title}</h3>
+                        <h3>{props.profileInfo.artist}</h3>
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
