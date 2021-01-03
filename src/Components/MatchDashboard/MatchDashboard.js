8aebf227d16caf37cd63b05f8e915230d7fec988
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-01-02 21:14:40 -0500

    populated profile with placeholder data and added arrow carousel functionality to iterate through profile

--- src/Components/MatchDashboard/MatchDashboard.js
+++ src/Components/MatchDashboard/MatchDashboard.js
@@ -3,40 +3,130 @@ import { Profile } from '../Profile/Profile';
 import './MatchDashboard.css';
 import playButton from '../../images/PlayButton(swiperight).png'
 import stopButton from '../../images/StopButton.png'
+import LeftArrow from '../../images/LeftArrow.png'
+import RightArrow from '../../images/RightArrow.png'
 
 export function MatchDashboard() {
 
-    const [profile, setProfile] = useState([]);
+    const [profiles, setProfiles] = useState([
+        {
+            id: 0,
+            slideTitle: 'theme song',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+            title: '"Adore You"',
+            artist: 'Harry Styles'
+        },
+        {
+            id: 1,
+            slideTitle: 'their playlist',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+            title: '"Adore You"',
+            artist: 'Harry Styles'
+        },
+        {
+            id: 2,
+            slideTitle: 'guess their favorite lyric',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+            title: '"Adore You"',
+            artist: 'Harry Styles'
+        },
+        {
+            id: 3,
+            slideTitle: 'what you both like',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+            title: '"Adore You"',
+            artist: 'Harry Styles'
+        },
+        {
+            id: 4,
+            slideTitle: '% match',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+            title: '"Adore You"',
+            artist: 'Harry Styles'
+        }
+    ]);
+    const [current, setCurrent] = useState(0);
 
-    useEffect(() => {
+    function fetchData() {
         try {
             const profileInfo = [
                 {
-                    id:1,
+                    id: 0,
                     slideTitle: 'theme song',
+                    image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+                    title: '"Adore You"',
+                    artist: 'Harry Styles'
+                },
+                {
+                    id: 1,
+                    slideTitle: 'their playlist',
+                    image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+                    title: '"Adore You"',
+                    artist: 'Harry Styles'
+                },
+                {
+                    id: 2,
+                    slideTitle: 'guess their favorite lyric',
+                    image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+                    title: '"Adore You"',
+                    artist: 'Harry Styles'
+                },
+                {
+                    id: 3,
+                    slideTitle: 'what you both like',
+                    image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
+                    title: '"Adore You"',
+                    artist: 'Harry Styles'
+                },
+                {
+                    id: 4,
+                    slideTitle: '% match',
                     image: 'https://images-na.ssl-images-amazon.com/images/I/61uo57hXGxL._SX522_.jpg',
                     title: '"Adore You"',
                     artist: 'Harry Styles'
                 }
             ]
-            setProfile(profileInfo);
+            setProfiles(profileInfo);
         } catch (error) {
             console.error(error.message);
         }
+    }
+
+    const handleLeftArrowClick = () => {
+        setCurrent(current == 0 ? profiles.length - 1 : current - 1);
+    }
+
+    const handleRightArrowClick = () => {
+        setCurrent(current == profiles.length - 1 ? 0 : current + 1);
+    }
+
+    useEffect(() => {
+        //fetchData();
     }, [])
+
+
+    /*profiles.map(profile => {
+                        return <Profile profileInfo={profile}
+                        key={profile.id} />
+                    })
+                    
+                    <Profile profileInfo={profile[current]}
+                        key={profile[current].id} />*/
 
     return (
         <Fragment>
             <div className="MatchDashboard">
-                {
-                    profile.map(profile => {
-                    return <Profile profileInfo={profile}
-                    key={profile.id}/>})
-                    
-                }
-                <div>
+                <div className="profileSection">
+                    <img src={LeftArrow} onClick={handleLeftArrowClick}></img>
+
+                    <Profile profileInfo={profiles[current]}
+                        key={profiles[current].id} />
+
+                    <img src={RightArrow} onClick={handleRightArrowClick}></img>
+                </div>
+                <div className="nameSection">
                     <h1 className="name">Summer L.</h1>
-                    <div className = "buttons">
+                    <div className="buttons">
                         <img className="button" src={playButton}></img>
                         <img className="button" src={stopButton}></img>
                     </div>
