3104288c841faf37b7e88c89dec370317181fe94
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-01-04 10:58:59 -0500

    add state to dependency array to properly render % match first render

--- src/Components/Slides/SlidePercentMatch/SlidePercentMatch.js
+++ src/Components/Slides/SlidePercentMatch/SlidePercentMatch.js
@@ -10,36 +10,38 @@ export function SlidePercentMatch(props) {
     const [instrumentalnessDiff, setInstrumentalnessDiff] = useState();
     const [livenessDiff, setLivenessDiff] = useState();
     const [valenceDiff, setValenceDiff] = useState();
-    const [percentage, setPercentage] = useState();
-
-    const yourMusic = {
-        trackQualities: {acousticness: .7,
-        danceability: .78,
-        energy: .5,
-        instrumentalness: .2,
-        liveness: .3,
-        valence: .7},
+    const [percentage, setPercentage] = useState("");
+    const [yourMusic, setYourMusic] = useState({
+        trackQualities: {
+            acousticness: .7,
+            danceability: .78,
+            energy: .5,
+            instrumentalness: .2,
+            liveness: .3,
+            valence: .7
+        },
         tempo: 100
-    }
-    
+    });
+
+
     const calculateDifference = (num1, num2) => {
-        return Math.abs(num1-num2);
+        return Math.abs(num1 - num2);
     }
 
     const calculatePercentMatch = (yourOwnMusic, theirMusic) => {
+
         setAcousticnessDiff(calculateDifference(yourOwnMusic.trackQualities.acousticness, theirMusic.trackQualities.acousticness));
         setDanceabilityDiff(calculateDifference(yourOwnMusic.trackQualities.danceability, theirMusic.trackQualities.danceability));
         setEnergyDiff(calculateDifference(yourOwnMusic.trackQualities.energy, theirMusic.trackQualities.energy));
         setInstrumentalnessDiff(calculateDifference(yourOwnMusic.trackQualities.danceability, theirMusic.trackQualities.danceability));
         setLivenessDiff(calculateDifference(yourOwnMusic.trackQualities.liveness, theirMusic.trackQualities.liveness));
         setValenceDiff(calculateDifference(yourOwnMusic.trackQualities.valence, theirMusic.trackQualities.valence));
-
-        setPercentage(Math.floor((1 - ((acousticnessDiff + danceabilityDiff + energyDiff + instrumentalnessDiff + livenessDiff + valenceDiff) / 6)) * 100 )+ "%")
     }
 
     useEffect(() => {
-        calculatePercentMatch(yourMusic, props.profileInfo)
-    }, [])
+        calculatePercentMatch(yourMusic, props.profileInfo);
+        setPercentage(Math.floor((1 - ((acousticnessDiff + danceabilityDiff + energyDiff + instrumentalnessDiff + livenessDiff + valenceDiff) / 6)) * 100) + "%")
+    }, [acousticnessDiff, danceabilityDiff, energyDiff, instrumentalnessDiff, livenessDiff, valenceDiff])
 
     return (
         <Fragment>
