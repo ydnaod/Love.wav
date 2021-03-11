a5442213aa4b31c31f84ee2e6401eb7456dbb1c1
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-03-11 12:52:02 -0500

    use useRef hook instead of useState hook for aduio object

--- src/Components/AudioObject/AudioObject.js
+++ src/Components/AudioObject/AudioObject.js
@@ -1,28 +1,30 @@
-import React, {useState, Fragment, useEffect} from 'react';
+import React, {useState, Fragment, useEffect, useRef} from 'react';
 
 export function AudioObject({isPlayingSample}) {
 
-    const [song, setSong] = useState()
+    //const [song, setSong] = useState()
+    const song = useRef(new Audio(isPlayingSample));
 
     useEffect(() => {
 
-        let audioObject;
+        //let audioObject;
         if (isPlayingSample !== false) {
-            audioObject = new Audio(isPlayingSample);
-            setSong(audioObject);
+            const audioObject = new Audio(isPlayingSample);
+            // setSong(audioObject);
 
            //setInterval(() => {this.setState({isPlaying: false})}, 30000)
+
            console.log(song);
            console.log(audioObject);
-           song.play();
+           song.current.play();
            //setTimeout(() => {song.play();}, 10000);
            //setIsPlayingSample(isPlayingSample);
            }
         else if (isPlayingSample) {
-            audioObject.pause();
+            song.current.pause();
         }
         else {
-            audioObject.pause();
+            song.current.pause();
             //setIsPlayingSample(false);
         }
     },[isPlayingSample])
