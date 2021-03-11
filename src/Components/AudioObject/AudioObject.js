39a5654fc1121cef6d611d508db1c823ac7de83a
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-03-10 19:03:15 -0500

    add state for audioobject

--- src/Components/AudioObject/AudioObject.js
+++ src/Components/AudioObject/AudioObject.js
@@ -2,22 +2,30 @@ import React, {useState, Fragment, useEffect} from 'react';
 
 export function AudioObject({isPlayingSample}) {
 
-
+    const [song, setSong] = useState()
 
     useEffect(() => {
 
         let audioObject;
-        if (isPlayingSample) {
+        if (isPlayingSample !== false) {
             audioObject = new Audio(isPlayingSample);
+            setSong(audioObject);
+
            //setInterval(() => {this.setState({isPlaying: false})}, 30000)
-           audioObject.play();
+           console.log(song);
+           console.log(audioObject);
+           song.play();
+           //setTimeout(() => {song.play();}, 10000);
            //setIsPlayingSample(isPlayingSample);
            }
-           else {
-             audioObject.pause();
-             //setIsPlayingSample(false);
-           }
-    },[])
+        else if (isPlayingSample) {
+            audioObject.pause();
+        }
+        else {
+            audioObject.pause();
+            //setIsPlayingSample(false);
+        }
+    },[isPlayingSample])
 
     return (
         <Fragment>
